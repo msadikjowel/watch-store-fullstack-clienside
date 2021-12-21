216c5bc8336bd83767ac8dc500572554e6a390a9
Mohammed Sadik <msadikjowel@gmail.com>
2021-12-21 12:36:16 +0600

    category parts added in home section

---
+++ src/Pages/Home/Categories/Categories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Categories.css'
+import appleWatch from './../../../img/appleW.png'
+import samsungWatch from './../../../img/samsung.png'
+import googleWatch from './../../../img/google.png'
+import allWatch from './../../../img/all.png'
+import { HashLink as Link } from 'react-router-hash-link';
+
+const Categories = () => {
+    return (
+        <div className='categories container text-center mt-5'>
+            <h2 className='category-title'>EXPLORE CATEGORIES</h2>
+            <div className="images">
+
+                <Link to='/allServices' className='navlink'> <div className="img">
+                    <img className='img-fluid' src={appleWatch} alt="" />
+                    <h3>Apple Watch</h3>
+                </div></Link>
+
+                <Link to='/allServices' className='navlink'><div className="img">
+                    <img className='img-fluid' src={samsungWatch} alt="" />
+                    <h3>Samsung Watch</h3>
+                </div></Link>
+                <Link to='/allServices' className='navlink'><div className="img">
+                    <img className='img-fluid' src={googleWatch} alt="" />
+                    <h3>Google Watch</h3>
+                </div></Link>
+                <Link to='/allServices' className='navlink'> <div className="img">
+                    <img className='img-fluid' src={allWatch} alt="" />
+                    <h3>Shop All Watches</h3>
+                </div></Link>
+
+            </div>
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
+import Categories from '../Categories/Categories';
 import Deals from '../Deals/Deals';
 import Feature from '../Feature/Feature';
 import HomeServices from '../HomeServices/HomeServices';
@@ -17,6 +18,7 @@ const Home = () => {
             <Feature></Feature>
             <WatchVideo></WatchVideo>
             <Deals></Deals>
+            <Categories></Categories>
             <Reviews></Reviews>
             <Footer></Footer>
         </div>
