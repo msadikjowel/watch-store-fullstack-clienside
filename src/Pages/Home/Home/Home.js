e5ce1fd518d542d6e6e0f7563dfa8a2eff211282
Mohammed Sadik <msadikjowel@gmail.com>
2022-02-17 23:17:17 +0600

    page loading spinner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
@@ -8,19 +8,38 @@ import Feature from '../Feature/Feature';
 import HomeServices from '../HomeServices/HomeServices';
 import Reviews from '../Reviews/Reviews';
 import WatchVideo from '../WatchVideo/WatchVideo';
+import ClockLoader from "react-spinners/ClockLoader";
+
+
 
 const Home = () => {
+    // page loader
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 1200);
+    }, [])
+
     return (
         <div>
-            <Header></Header>
-            <Banner></Banner>
-            <HomeServices></HomeServices>
-            <Feature></Feature>
-            <WatchVideo></WatchVideo>
-            <Deals></Deals>
-            <Categories></Categories>
-            <Reviews></Reviews>
-            <Footer></Footer>
+            {
+                loading ? <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100%', height: '100vh' }}><ClockLoader color={'#002d74'} loading={loading} size={50} /></div>
+                    :
+
+                    <>
+
+                        <Header></Header>
+                        <Banner></Banner>
+                        <HomeServices></HomeServices>
+                        <Feature></Feature>
+                        <WatchVideo></WatchVideo>
+                        <Deals></Deals>
+                        <Categories></Categories>
+                        <Reviews></Reviews>
+                        <Footer></Footer>
+                    </>
+            }
         </div>
     );
 };
